d20e17d4aaea18cf545485c8c4a223908a19c57b
marcopenso01 <45823432+marcopenso01@users.noreply.github.com>
2020-01-22 17:26:17 +0100

    Update metrics.py

--- metrics.py
+++ metrics.py
@@ -53,13 +53,43 @@ def compute_metrics_on_directories_raw(dir_gt, dir_pred):
             dir_ph_gt = os.path.join(dir_p_gt, phase_gt)
             dir_ph_pred = os.path.join(dir_p_pred, phase_pred)
             
-            for img_gt, img_pred in zip(sorted(glob.glob(dir_ph_gt)), sorted(glob.glob(dir_ph_pred))):
-                if (img_gt != img_pred):
-                    raise ValueError("The two images don't have the same name"
-                                     " {}, {}.".format(img_gt, img_pred))
-                gt_addr = os.path.join(dir_ph_gt, img_gt)
-                pred_addr = os.path.join(dir_ph_pred, img_pred)
+            for struc in [3,1,2]:
                 
+                volpred = 0
+                volgt = 0
+                for img_gt, img_pred in zip(sorted(glob.glob(dir_ph_gt)), sorted(glob.glob(dir_ph_pred))):
+                    if (img_gt != img_pred):
+                        raise ValueError("The two images don't have the same name"
+                                         " {}, {}.".format(img_gt, img_pred))
+                    gt_addr = os.path.join(dir_ph_gt, img_gt)
+                    pred_addr = os.path.join(dir_ph_pred, img_pred)
+                    gt = cv2.imread(gt_addr,0)
+                    pred = cv2.imread(pred_addr,0)
+
+                    gt_binary = (gt == struc) * 1
+                    pred_binary = (pred == struc) * 1
+                    
+                    volpred = volpred + (pred_binary.sum() * config.z_dim)
+                    volgt = volgt + (gt_binary.sum() * config.z_dim)
+                
+                vol_list.append(volpred)
+                vol_err_list.append(volpred - volgt)
+                vol_gt_list.append(volgt)
+                
+                if gt_binary.sum() == 0 and pred_binary.sum() == 0:
+                    dices_list.append(1)
+                    hausdorff_list.append(0)
+                elif pred_binary.sum() > 0 and gt_binary.sum() == 0 or pred_binary.sum() == 0 and gt_binary.sum() > 0:
+                    logging.warning('Structure missing in either GT (x)or prediction. ASSD and HD will not be accurate.')
+                    dices_list.append(0)
+                    hausdorff_list.append(1)
+                else:
+                    hausdorff_list.append(bm.hd(gt_binary, pred_binary, voxelspacing=zooms, connectivity=1))
+                    assd_list.append(bm.assd(pred_binary, gt_binary, voxelspacing=zooms, connectivity=1))
+                    dices_list.append(bm.dc(gt_binary, pred_binary))
+            
+            
+
 
 def main(path_pred, path_gt, eval_dir):
     logging.info(path_gt)
