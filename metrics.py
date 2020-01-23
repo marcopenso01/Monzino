1d3700f7fc8281be027515eb7b6bceea94a05cd4
marcopenso01 <45823432+marcopenso01@users.noreply.github.com>
2020-01-23 09:31:43 +0100

    Update metrics.py

--- metrics.py
+++ metrics.py
@@ -13,6 +13,104 @@ import seaborn as sns
 
 import logging
 
+def conv_int(i):
+    return int(i) if i.isdigit() else i
+
+
+def natural_order(sord):
+    """
+    Sort a (list,tuple) of strings into natural order.
+    Ex:
+    ['1','10','2'] -> ['1','2','10']
+    ['abc1def','ab10d','b2c','ab1d'] -> ['ab1d','ab10d', 'abc1def', 'b2c']
+    """
+    if isinstance(sord, tuple):
+        sord = sord[0]
+    return [conv_int(c) for c in re.split(r'(\d+)', sord)]
+
+
+def print_latex_tables(df, eval_dir):
+    """
+    Report geometric measures in latex tables to be used in the ACDC challenge paper.
+    Prints mean (+- std) values for Dice for all structures.
+    :param df:
+    :param eval_dir:
+    :return:
+    """
+
+    out_file = os.path.join(eval_dir, 'latex_tables.txt')
+
+    with open(out_file, "w") as text_file:
+
+        text_file.write('\n\n-------------------------------------------------------------------------------------\n')
+        text_file.write('table 1\n')
+        text_file.write('-------------------------------------------------------------------------------------\n\n')
+        # prints mean (+- std) values for Dice, all structures, averaged over both phases.
+
+        header_string = ' & '
+        line_string = 'METHOD '
+
+
+        for s_idx, struc_name in enumerate(['LV', 'RV', 'Myo']):
+            for measure in ['dice']:
+
+                header_string += ' & {} ({}) '.format(measure, struc_name)
+
+                dat = df.loc[df['struc'] == struc_name]
+
+                if measure == 'dice':
+                    line_string += ' & {:.3f}\,({:.3f}) '.format(np.mean(dat[measure]), np.std(dat[measure]))
+                else:
+                    line_string += ' & {:.2f}\,({:.2f}) '.format(np.mean(dat[measure]), np.std(dat[measure]))
+
+            if s_idx < 2:
+                header_string += ' & '
+                line_string += ' & '
+
+        header_string += ' \\\\ \n'
+        line_string += ' \\\\ \n'
+
+        text_file.write(header_string)
+        text_file.write(line_string)
+
+
+        text_file.write('\n\n-------------------------------------------------------------------------------------\n')
+        text_file.write('table 2\n')
+        text_file.write('-------------------------------------------------------------------------------------\n\n')
+        # table 2: mean (+- std) values for Dice and HD, all structures, both phases separately
+
+
+        for idx, struc_name in enumerate(['LV', 'RV', 'Myo']):
+            # new line
+            header_string = ' & '
+            line_string = '({}) '.format(struc_name)
+
+            for p_idx, phase in enumerate(['ED', 'ES']):
+                for measure in ['dice', 'hd']:
+
+                    header_string += ' & {} ({}) '.format(phase, measure)
+
+                    dat = df.loc[(df['phase'] == phase) & (df['struc'] == struc_name)]
+
+                    if measure == 'dice':
+                        line_string += ' & {:.3f}\,({:.3f}) '.format(np.mean(dat[measure]), np.std(dat[measure]))
+                    else:
+                        line_string += ' & {:.2f}\,({:.2f}) '.format(np.mean(dat[measure]), np.std(dat[measure]))
+
+                if p_idx == 0:
+                    header_string += ' & '
+                    line_string += ' & '
+
+            header_string += ' \\\\ \n'
+            line_string += ' \\\\ \n'
+
+            if idx == 0:
+                text_file.write(header_string)
+
+            text_file.write(line_string)
+
+    return 0
+
 
 def compute_metrics_on_directories_raw(dir_gt, dir_pred):
     '''
@@ -98,7 +196,90 @@ def compute_metrics_on_directories_raw(dir_gt, dir_pred):
     
     return df
     
+
+def print_stats(df, eval_dir):
     
+    out_file = os.path.join(eval_dir, 'summary_report.txt')
+    
+    with open(out_file, "w") as text_file:
+
+        text_file.write('\n\n-------------------------------------------------------------------------------------\n')
+        text_file.write('Summary of geometric evaluation measures. \n')
+        text_file.write('-------------------------------------------------------------------------------------\n\n')
+
+        for struc_name in ['LV', 'RV', 'Myo']:
+
+            text_file.write(struc_name)
+            text_file.write('\n')
+
+            for cardiac_phase in ['ED', 'ES']:
+
+                text_file.write('    {}\n'.format(cardiac_phase))
+
+                dat = df.loc[(df['phase'] == cardiac_phase) & (df['struc'] == struc_name)]
+
+                for measure_name in ['dice', 'hd']:
+
+                    text_file.write('       {} -- mean (std): {:.3f} ({:.3f}) \n'.format(measure_name,
+                                                                         np.mean(dat[measure_name]), np.std(dat[measure_name])))
+
+                    ind_med = np.argsort(dat[measure_name]).iloc[len(dat[measure_name])//2]
+                    text_file.write('             median {}: {:.3f} ({})\n'.format(measure_name,
+                                                                dat[measure_name].iloc[ind_med], dat['filename'].iloc[ind_med]))
+
+                    ind_worst = np.argsort(dat[measure_name]).iloc[0]
+                    text_file.write('             worst {}: {:.3f} ({})\n'.format(measure_name,
+                                                                dat[measure_name].iloc[ind_worst], dat['filename'].iloc[ind_worst]))
+
+                    ind_best = np.argsort(dat[measure_name]).iloc[-1]
+                    text_file.write('             best {}: {:.3f} ({})\n'.format(measure_name,
+                                                                dat[measure_name].iloc[ind_best], dat['filename'].iloc[ind_best]))
+
+
+        text_file.write('\n\n-------------------------------------------------------------------------------------\n')
+        text_file.write('Ejection fraction correlation between prediction and ground truth\n')
+        text_file.write('-------------------------------------------------------------------------------------\n\n')
+
+        for struc_name in ['LV', 'RV']:
+
+            lv = df.loc[df['struc'] == struc_name]
+
+            ED_vol = np.array(lv.loc[lv['phase'] == 'ED']['vol'])
+            ES_vol = np.array(lv.loc[(lv['phase'] == 'ES')]['vol'])
+            EF_pred = (ED_vol - ES_vol) / ED_vol
+
+            ED_vol_gt = ED_vol - np.array(lv.loc[lv['phase'] == 'ED']['vol_err'])
+            ES_vol_gt = ES_vol - np.array(lv.loc[(lv['phase'] == 'ES')]['vol_err'])
+
+            EF_gt = (ED_vol_gt - ES_vol_gt) / ED_vol_gt
+
+            LV_EF_corr = stats.pearsonr(EF_pred, EF_gt)
+            text_file.write('{}, EF corr: {}\n\n'.format(struc_name, LV_EF_corr[0]))
+
+
+def boxplot_metrics(df, eval_dir):
+    """
+    Create summary boxplots of all geometric measures.
+    :param df:
+    :param eval_dir:
+    :return:
+    """
+
+    boxplots_file = os.path.join(eval_dir, 'boxplots.eps')
+
+    fig, axes = plt.subplots(2, 1)
+    fig.set_figheight(14)
+    fig.set_figwidth(7)
+
+    sns.boxplot(x='struc', y='dice', hue='phase', data=df, palette="PRGn", ax=axes[0])
+    sns.boxplot(x='struc', y='hd', hue='phase', data=df, palette="PRGn", ax=axes[1])
+    
+    plt.savefig(boxplots_file)
+    plt.close()
+
+    return 0
+    
+
 def main(path_pred, path_gt, eval_dir):
     logging.info(path_gt)
     logging.info(path_pred)
