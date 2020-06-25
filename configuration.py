b3365a17d73a9c5bd52f1d3fb06cf58d92b59b1f
marcopenso01 <45823432+marcopenso01@users.noreply.github.com>
2020-06-25 18:26:04 +0200

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -40,7 +40,7 @@ split_test_train = True   #divide patients in train and test. If true define spl
 split = 5                 #  2: 50% train and 50% validation,    5: 80% training, 20% validation
 train_on_all_data = False 
 gt_exists = True    #True if it exists the ground_trth images, otherwise False.
-z_dim = 1
+z_dim = 8
 
 # Training settings
 batch_size = 4      #4 
